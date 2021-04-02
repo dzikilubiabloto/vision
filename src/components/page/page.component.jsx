a6ef24d37a23aabd3b90b2b08d5fe9ad7d283808
agatatalita <agatamonikaandrzejewska@gmail.com>
2021-04-02 16:28:39 +0200

    add terms page

--- src/components/page/page.component.jsx
+++ src/components/page/page.component.jsx
@@ -1,38 +1,16 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Books from "../books/books.component";
 import Dreaming from "../dreaming/dreaming.component";
 import Meeting from "../meeting/meeting.component";
+import Terms from "../terms/terms.component";
 import Values from "../values/values.component";
 
 import "./page.styles.scss";
 
-function Page({ chosenPage }) {
-  console.log("cccc", chosenPage);
-  // switch(chosenPage) {
-
-  //     case('home'):{
-  //         return (<div></div>);
-  //     };
-  //     case('values'): {
-  //         console.log("UUUUUUUUUUUUUUUUUU")
-  //         return <Values />
-  //     };
-  //     case('dreaming'): {
-  //         console.log("UUUUUUUUUUUUUUUUUU")
-  //         return <Dreaming />
-  //     };
-  //     case('books'): {
-  //         console.log("UUUUUUUUUUUUUUUUUU")
-  //         return <Books />
-  //     };
-  //     case('meeting'): {
-  //         console.log("UUUUUUUUUUUUUUUUUU")
-  //         return <Meeting />
-  //     };
-  // }
+function Page() {
   return (
     <div>
       <Switch>
@@ -52,6 +30,9 @@ function Page({ chosenPage }) {
           <Meeting />
         </Route>
       </Switch>
+      <Route exact path="/meeting">
+        <Terms />
+      </Route>
     </div>
   );
 }
---
+++ src/components/terms/terms.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./terms.styles.scss";
+
+function Terms() {
+  return (
+    <div className="values-container">
+      <div className="values">Terms</div>
+    </div>
+  );
+}
+
+export default Terms;
