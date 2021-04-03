c00fd103018302824e9eb1108aac8b68a80a59fe
agatatalita <agatamonikaandrzejewska@gmail.com>
2021-04-03 21:31:41 +0200

    add data to books page

--- src/components/books/book/book.component.jsx
+++ src/components/books/book/book.component.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 import "./book.styles.scss";
 
-function Book({book}) {
-
-  const {image, title, author, types, description} = book;
+function Book({ book }) {
+  const { image, title, author, types, description, link } = book;
   return (
     <div className="book-container">
-        <div className="book-image">
-            <img src={image} alt={title + " book cover"} />
+      <div className="book-image">
+        <a href={link} rel="noreferrer noopener" target="_blank">
+          <img src={image} alt={title + " book cover"} />
+        </a>
+      </div>
+      <div className="book-data-container">
+        <div className="book-data">
+          <div className="title">{title}</div>
+          <div className="author">{author}</div>
+          <div className="types">{types}</div>
+          <div className="description">{description}</div>
         </div>
-        <div className="book-data-container">
-            <div className="book-data">
-                <div>{title}</div>
-                <div>{author}</div>
-                <div>{types}</div>
-                <div>{description}</div>
-            </div>
-        </div>
+      </div>
     </div>
   );
 }
--- src/components/books/books.component.jsx
+++ src/components/books/books.component.jsx
@@ -7,32 +7,166 @@ import Book from "./book/book.component";
 const books = [
   {
     image: "https://i.ibb.co/pXTtV7S/clt.jpg",
+    link: "https://www.goodreads.com/book/show/232438.Creating_a_Life_Together",
     title:
       "Creating a Life Together: Practical Tools to Grow Ecovillages and Intentional Communities",
     author: "Diana Leafe Christian",
     types: "I can share EBOOK and AUDIOBOOK",
-    description: "Book provides practical information on ho3 to build a community. ritten by an editor of 'Community' magazine",
-  },{
-    image: "https://i.ibb.co/pXTtV7S/clt.jpg",
-    title:
-      "Creating a Life Together: Practical Tools to Grow Ecovillages and Intentional Communities",
-    author: "Diana Leafe Christian",
-    description: "Book provides practical information on ho to build a community. ritten by an editor of 'Community' magazine",
+    description:
+      "Book provides practical information on how to build a community. Written by an editor of 'Communities' magazine",
+  },
+  {
+    image: "https://i.ibb.co/vmHgZjJ/51-Khd-Qn-Mtw-L-SX382-BO1-204-203-200.jpg",
+    link:
+      "https://www.goodreads.com/book/show/5718094-beyond-you-and-me?from_search=true&from_srp=true&qid=Xg8UhmIXNX&rank=1",
+    title: "Beyond You And Me: Inspiration And Wisdom For Community Building",
+    author: " Kosha Anja Joubert (Editor), Robin Alfred (Editor)",
+    types: "I can share EBOOK",
+    description:
+      "Articles - tips on topics of building community and living in a community. Less practical (building, money, land) and focused on connections, feelings. Articles by different authors.",
+  },
+  {
+    image: "https://i.ibb.co/QD2rcbg/download.jpg",
+    link:
+      "https://www.goodreads.com/book/show/4443.The_Different_Drum?from_search=true&from_srp=true&qid=scxtJ0Rac4&rank=1",
+    title: "The Different Drum: Community Making and Peace",
+    author: " M. Scott Peck",
+    types: "I can share EBOOK",
+    description:
+      "Psychological approach to community feeling. 4 steps of community creation (pseudocommunity, chaos, emptiness, community) described in this book.",
+  },
+];
+
+const books2 = [
+  {
+    image: "https://i.ibb.co/yPL8fgF/Wisdom1-e1519323194233-200x254.png",
+    link:
+      "https://www.ic.org/community-bookstore/product/starting-a-community/",
+    title: "Wisdom of Communities: Volume 1 – Starting a Community",
+    author: "articles from 'Communities magazine', various authors",
+    types: "I can share EBOOK",
+    description:
+      "Volume 1 of the Wisdom of Communities series includes both general articles and on-the-ground stories from intentional community founders and other catalysts of cooperative efforts. This book aims to increase the survival rate of attempts to start intentional communities (it’s estimated that currently just 10 percent move past the initial stages).",
+  },
+  {
+    image: "https://i.ibb.co/8MqLgvf/Wisdom2-200x252.png",
+    link: "https://www.ic.org/community-bookstore/product/finding-a-community/",
+    title: "Wisdom of Communities: Volume 2 – Finding a Community",
+    author: "articles from 'Communities magazine', various authors",
+    types: "I can share EBOOK",
+    description:
+      "'In Volume 2 “Finding a Community” we respond to the reality that many searches for intentional community fizzle out due to lack of adequate information, guidance, or exposure to fellow travelers’ stories. Authors share experiences, tools, advice, and perspectives that should help anyone searching for an intentional community—whether to visit or to live in—increase the likelihood of finding what they’re seeking.'",
+  },
+  {
+    image: "https://i.ibb.co/yhgcjvk/Wisdom3-e1519323269676-235x300.png",
+    link:
+      "https://www.ic.org/community-bookstore/product/communication-in-community/",
+    title: "Wisdom of Communities: Volume 3 – Communication in Community",
+    author: "articles from 'Communities magazine', various authors",
+    types: "I can share EBOOK",
+    description:
+      "Volume 3 “Communication in Community” includes articles about decision-making, governance, power, gender, class, race, relationships, intimacy, politics, and neighbor relations in cooperative group culture. These areas are key for communities to address if they are to retain members and develop strong and healthy group connection.",
+  },
+  {
+    image:
+      "https://i.ibb.co/jfgcHqM/Wisdom-4-Sustainability-in-Community-Front-Cover-200x258.png",
+    link:
+      "https://www.ic.org/community-bookstore/product/sustainability-in-community/",
+    title: "Wisdom of Communities: Volume 4 – Sustainability in Community",
+    author: "articles from 'Communities magazine', various authors",
+    types: "I can share EBOOK",
+    description:
+      "In Volume 4 “Sustainability in Community” we focus on food, water, shelter, energy, land, permaculture, ecovillage design, eco-education, and resilience in cooperative culture. These areas will prove more and more essential in allowing communities to navigate changing circumstances on our planet, while growing into new, regenerative ways of living and thriving together.",
+  },
+  {
+    image: "https://i.ibb.co/PgR9KqC/gaian-economics.jpg",
+    link: "https://www.gaiaeducation.org/product/gaian-economics/",
+    title: "Gaian Economics",
+    author:
+      "Jonathan Dawson  (Editor), Ross Jackson  (Editor), Helena Norberg-Hodge (Editor)",
+    types: "I can share EBOOK",
+    description:
+      "Gaian Economics: Living Well within Planetary Limits (The Economic Key) is the second in the series of Four Keys that supplement the Curriculum. This particular book is for anyone seeking solutions to the economic problems facing both global society and local communities in a sustainable world.",
   },
 ];
 
 function Books() {
   return (
     <div className="values-container">
-      <div className="values">
+      <div className="values books">
         <img src="https://i.ibb.co/bNcZCLZ/pexels-julia-volk-527307nnn6.jpg" />
-        https://ecovillage.org/
-        https://www.ic.org/community-bookstore/product/the-cohouseholding-guide/
-        <div className="books-list">
-          {books.map((book) => (
-            <Book book={book} />
-          ))}
-          https://library.uniteddiversity.coop/Effective_Organising/
+        <div className="books-content">
+          <div className="books-title">
+            <div>Books - I can share and I read</div>
+          </div>
+
+          <div className="books-list">
+            {books.map((book) => (
+              <Book book={book} />
+            ))}
+          </div>
+          <div className="books-title">
+            <div>
+              Books - I can share and I am currently reading/going to read
+            </div>
+          </div>
+
+          <div className="books-list">
+            {books2.map((book) => (
+              <Book book={book} />
+            ))}
+          </div>
+          <div className="books-title">
+            <div>Websites:</div>
+          </div>
+
+          <div className="websites">
+            <div className="website">
+              <a href="https://ecovillage.org/">https://ecovillage.org/ </a>
+            </div>
+          </div>
+          <div className="websites">
+            <div className="website">
+              <a href="https://www.ic.org/">https://www.ic.org</a>
+            </div>
+          </div>
+          <div className="websites">
+            <div className="website">
+              <a href="https://www.ic.org/community-bookstore/product/the-cohouseholding-guide/">
+                https://www.ic.org/directory/ (map of communities)
+              </a>
+            </div>
+          </div>
+          <div className="websites">
+            <div className="website">
+              <a href="https://ecovillage.org/">https://ecovillage.org/ </a>
+            </div>
+          </div>
+          <div className="websites">
+            <div className="website">
+              <a href="https://library.uniteddiversity.coop/Effective_Organising/">
+                https://library.uniteddiversity.coop/Effective_Organising/
+              </a>
+            </div>
+          </div>
+          <div className="websites">
+            <div className="website">
+              <a href="http://www.rivendellvillage.org/index.html">
+                http://www.rivendellvillage.org/index.html (some DIY materials)
+              </a>
+            </div>
+          </div>
+
+          <div className="books-title">
+            <div>Communities:</div>
+          </div>
+          <div className="websites">
+            <div className="website">
+              <a href="https://www.dancingrabbit.org/">
+                https://www.dancingrabbit.org/{" "}
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
