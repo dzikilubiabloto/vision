dc3c21dcf682edadee89e8ebba4f3ddb05073772
agatatalita <agatamonikaandrzejewska@gmail.com>
2021-04-05 16:58:50 +0200

    create dreaming page and add data

--- src/components/dreaming/dreaming.component.jsx
+++ src/components/dreaming/dreaming.component.jsx
@@ -8,23 +8,268 @@ function Dreaming() {
       <div className="values dreaming">
         <iframe
           title="listening"
-          src="https://player.vimeo.com/video/227126380"
+          src="https://player.vimeo.com/video/227126380?color=ffffff&title=0&byline=0&portrait=0"
           width="640"
           height="360"
           frameborder="0"
           allow="autoplay; fullscreen; picture-in-picture"
           allowfullscreen
         ></iframe>
-        
-        <iframe
-          title="dancing-rabbit"
-          src="https://player.vimeo.com/video/280254239?color=579c87"
-          width="640"
-          height="360"
-          frameborder="0"
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+
+        <div class="typora-export">
+          <div id="write" class="">
+            <h2>
+              <a
+                name="community---dreaming-vision"
+                class="md-header-anchor"
+              ></a>
+              <span>Community - dreaming, vision,</span>
+            </h2>
+            <h5>
+              <a name="sauna-lake-fire-and-garden" class="md-header-anchor"></a>
+              <span>Sauna, lake, fire and garden</span>
+            </h5>
+            <p>
+              <span>
+                When I think about a community I would like to be a part of, the
+                one I would like to build with other people, the first things I
+                feel are calmness, softness, and safety. I imagine relaxing in
+                the sauna and seeing that everybody is comfortable in the
+                silence we are sharing. A few people who are more connected
+                because they are building a new cob house for the last few
+                weeks, a person who is on a road to discovering what is
+                important to them, so spends last days alone in nature,
+                newcomers who arrived last week and are curiously exploring the
+                space.
+              </span>
+            </p>
+            <p>
+              <span>
+                I see our meeting by the fire, under the stars, which starts
+                with having fun and somehow transforms to sharing our family
+                stories which are still hard for us to think about, but somehow
+                it is lighter to think about them in this group. It ends up with
+                singing songs that hadn&#39;t been invented yet, using the
+                melody to express what we feel and connect to the melodies of
+                others. Or a few of us just joined a dinner table, covered with
+                colorful vegetables and a warm baked meal. In one part of the
+                table, there is a sparkling dreaming conversation about an idea
+                of a new workshop that could be organized using the knowledge we
+                already have and gathered in the last months. Maybe circus of
+                mathematicians and constructors? At the other place, somebody
+                shares a story from their youth, from 40 or 50 years back. It is
+                heartwarming and in some way empowering to hear this tale
+                celebrating friendship from so long time ago. And there is a
+                more tranquil corner, close to the bushes, one can see a lake
+                from there and listen to the birds twittering. What about
+                swimming just before the sunset, or maybe a short boat trip?
+              </span>
+            </p>
+            <h5>
+              <a name="pain-acceptance-space" class="md-header-anchor"></a>
+              <span>Pain, acceptance, space</span>
+            </h5>
+            <p>
+              <span>
+                I remember somebody just lost their mother, and some hearts are
+                broken for many reasons. If it happens to me, I know, by the
+                breakfast table or some other common gathering I find warmth
+                that supports the healing process. I can share the whole story
+                or if the information part is not so important for me - just how
+                I feel now. I can have my space, my behavior differs from how I
+                usually am. I am quieter, less responsive and I get irritated
+                more easily. I know people around me will understand it. I can
+                take my time or share a helpful talk or a boat trip.
+              </span>
+            </p>
+            <h5>
+              <a
+                name="conflicts-confusion-connection"
+                class="md-header-anchor"
+              ></a>
+              <span>Conflicts, confusion, connection</span>
+            </h5>
+            <p>
+              <span>
+                We have our internal struggles and conflicts. Last time we
+                couldn&#39;t decide what will be the purpose of the new
+                straw-bale house we built. There was some confusion with
+                informal decisions and some power imbalance between people who
+                built it and those who didn&#39;t. We were not sure it should be
+                a children&#39;s house or a new pottery workshop we really need
+                for the upcoming projects and we are not planning to start any
+                new building before wintertime. We use consensus for making
+                decisions and we spent some time talking about our
+                possibilities. It is sometimes hard but afterward usually
+                rewarding. We have conflicts. We may avoid each other for some
+                days but in the end, we know conflicts can bring us closer. It
+                is not a nice experience and sometimes we are really worn. We
+                are learning a lot about others but probably more about
+                ourselves. It helps that we have various life experiences and
+                diverse attitudes towards life. We share values we think are
+                important for our community and learn to live with our
+                differences. We want to appreciate them. Sometimes it is tough
+                to even see why, but when we get to a new choice, concept, based
+                on the mixture of all of our traits it is very gratifying. Or
+                when we understand somebody and learn to appreciate them and in
+                place of an itchy confusion arises that warm glowing sensation.
+              </span>
+            </p>
+            <h5>
+              <a
+                name="beauty-cooperation-joy-freedom"
+                class="md-header-anchor"
+              ></a>
+              <span>Beauty, cooperation, joy, freedom</span>
+            </h5>
+            <p>
+              <span>
+                {" "}
+                I also see beautiful workshops. For curiosity, freedom,
+                creativity, dreaming. Careful design, serving as well the
+                pleasure of looking at them, as a convenience and easy
+                navigation between all of the tools. It is soothing to look at
+                people working there, sometimes inspiring and motivating.
+                Sometimes somebody needs help and it is a particular opportunity
+                to learn or have fun together. It is more comfortable and faster
+                to start building something from wood or go back to a
+                not-finished painting that is drying there. There is a group of
+                kids building a piece of furniture for their new house. There is
+                a group of people studying wood joints to use for assembling a
+                treehouse. It looks like a school from dreams. We are from 1 to
+                100 years old, we can use each place and what is maybe more
+                important, if we don&#39;t like some subject, we don&#39;t have
+                to work on it, we may not visit the workshop once. And we see
+                each other failing frequently and leaving the area or trying
+                again. And we see it is ok to fail, it is a part of the process.
+              </span>
+            </p>
+            <h5>
+              <a
+                name="nature-books-neighbors-change"
+                class="md-header-anchor"
+              ></a>
+              <span>Nature, books, neighbors, change</span>
+            </h5>
+            <p>
+              <span>
+                In the workshops but also in the other areas, an orchard,
+                garden, on the grass fields or by the forest, or in the
+                naturally built houses are happening projects connecting us with
+                our local surroundings and bigger world. We are running a free
+                cafe library. Often we organize natural building and
+                permaculture classes and also workshops on topics as
+                &quot;activist regeneration&quot;, &quot;authenticity&quot; and
+                some dance seminars. Some of us would like to start a workshop
+                for building tiny houses with the use of recycled materials.
+                Some of us are designing children&#39;s books. We are
+                brainstorming about inventing ways to encourage people to build
+                more communities. Documentaries based on interviews with
+                activists from around the world are made. Some of us take a rest
+                after occupying forest or factory. Maybe we can start a new
+                movement? Or create a new cool lamp or book.
+              </span>
+            </p>
+            <div className="dreaming-picture-container"><img className="dreaming-picture" src="https://i.ibb.co/0QBmzQC/pexels-maria-orlova-4946439.jpg"></img></div>
+            <h5>
+              <a name="calmness-listening-harmony" class="md-header-anchor"></a>
+              <span>Calmness, listening, harmony</span>
+            </h5>
+            <p>
+              <span>
+                And there are trips to the forest to collect blueberries or
+                herbs. Walks in nature, peaceful evenings in the garden.
+                Together, sharing joy or pain, listening, asking for advice or
+                presence. In solitude, taking time to calm down the thoughts, to
+                get harmony. Sun is setting down, the scent of the evening stock
+                is in the air. There is a space for all of it.
+              </span>
+            </p>
+            <p>&nbsp;</p>
+            <iframe
+              title="dancing-rabbit"
+              src="https://player.vimeo.com/video/280254239?color=579c87"
+              width="640"
+              height="360"
+              frameborder="0"
+              allow="autoplay; fullscreen; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+            <h2>
+              <a
+                name="community---technical-things"
+                class="md-header-anchor"
+              ></a>
+              <span>Community - technical things</span>
+            </h2>
+            <p>
+              <span>
+                Things I have in mind and should be talked about before building
+                a community are:
+              </span>
+            </p>
+            <ul>
+              <li>
+                <span>
+                  where will it be: which country, how far from the city,
+                  mountains, lake or floodplain, or something else
+                </span>
+              </li>
+              <li>
+                <span>
+                  where will the money come from? How much money will be needed
+                  to build things, to get food, live for a year
+                </span>
+              </li>
+              <li>
+                <span>
+                  what will be the rules, what will be the decision-making
+                  process, how many things we need to have written down to be
+                  able to live in harmony?{" "}
+                </span>
+              </li>
+              <li>
+                <span>
+                  who will be doing what kind of work and how much of it{" "}
+                </span>
+              </li>
+              <li>
+                <span>
+                  what are the rules of accepting new members and rules about
+                  leaving the community{" "}
+                </span>
+              </li>
+              <li>
+                <span>which projects we want to focus on first </span>
+              </li>
+              <li>
+                <span>what we want to build, when and how</span>
+              </li>
+              <li>
+                <span>how are we handling conflicts </span>
+              </li>
+              <li>
+                <span>other questions I hope we discover together </span>
+              </li>
+              <li>
+                <span>...</span>
+              </li>
+            </ul>
+            <p>
+              <em>
+                <span>
+                  I will be including more questions here while I am reading new
+                  books and articles. We can build our on list of questions
+                  during the meetings.
+                </span>
+              </em>
+            </p>
+            <p>&nbsp;</p>
+            <div className="dreaming-picture-container">
+            <img src="https://images.pexels.com/photos/712876/pexels-photo-712876.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
