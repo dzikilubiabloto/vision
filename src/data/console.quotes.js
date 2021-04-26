b2a1da0310ced566a3c9726d8d12ec1151c15725
agatatalita <agatamonikaandrzejewska@gmail.com>
2021-04-26 11:44:35 +0200

    add console quotes

--- src/data/console.quotes.js
+++ src/data/console.quotes.js
@@ -1,10 +1,12 @@
 const quotes = [
   [
-    `She would smile and show no surprise, convinced as she was, the same as I, that casual meetings are apt to be just the opposite, and that people who make dates are the same kind who need lines on their writing paper, or who always squeeze up from the bottom on a tube of toothpaste.`,
+    `She would smile and show no surprise, convinced as she was, the same as I, 
+that casual meetings are apt to be just the opposite, and that people who make dates are the same kind
+who need lines on their writing paper, or who always squeeze up from the bottom on a tube of toothpaste.`,
     "Julio Cortazar, Hopscotch",
   ],
-  ["", ""],
-  ["", ""],
+  ["Memory is a mirror that scandalously lies.", "Julio Cortazar"],
+  ["All profound distraction opens certain doors. You have to allow yourself to be distracted when you are unable to concentrate.", "Julio Cortazar"],
   ["", ""],
   ["", ""],
   ["", ""],
