5d8f25f0320ccf40dcfea3ff54f5dd94fb234257
agatatalita <agatamonikaandrzejewska@gmail.com>
2021-05-16 13:00:37 +0200

    adjust documents styling, layout

--- src/components/documents/documents-data/documents-data.component.jsx
+++ src/components/documents/documents-data/documents-data.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import ReactMarkdown from "react-markdown";
 import StringCrypto from "string-crypto";
+import Toast from "react-bootstrap/Toast";
 
 import gfm from "remark-gfm";
 
@@ -9,11 +10,14 @@ import { useAuth } from "../../../context/auth.context";
 import { saveDocument } from "../../../firebase/documents.utils";
 
 import "./documents-data.styles.scss";
+import { Container, Col, Row, Button, Alert } from "react-bootstrap";
+const re = /<!--((.|[\n|\r|\r\n])*?)-->[\n|\r|\r\n]?(\s+)?/g;
 
 function DocumentsData({ document, changeDocumentField }) {
   const { text, id } = document || { text: "", id: "" };
   const password = "./documents-data.styles.scss/llllkhhhnbn/";
   const [editing, setEditing] = React.useState(false);
+  const [showToastSaved, setShowToastSaved] = React.useState(false);
 
   const onTextChange3 = async (event) => {
     await changeDocumentField(event.target.value);
@@ -22,10 +26,11 @@ function DocumentsData({ document, changeDocumentField }) {
 
   const { currentUser } = useAuth();
 
-  const save = () => {
+  const save = async () => {
     let encryptedString = encryptString(text, password);
 
-    saveDocument(id, encryptedString, true);
+    await saveDocument(id, encryptedString, true);
+    await setShowToastSaved(true);
   };
 
   React.useEffect(() => {
@@ -40,47 +45,55 @@ function DocumentsData({ document, changeDocumentField }) {
   };
 
   return (
-    <div className="document-container-wrapper">
-      <div>
-        {currentUser && !editing && (
-          <div
-            className="document-edit-button"
-            onClick={() => toggleEditing(true)}
-          >
-            edit
-          </div>
-        )}
-        {currentUser && editing && (
-          <div
-            className="document-edit-button"
-            onClick={() => toggleEditing(false)}
-          >
-            stop editing and save
-          </div>
-        )}
-      </div>
-      <div className="document-container">
-        {currentUser && editing && <button onClick={save}>SAVE</button>}
-        {currentUser && editing && (
-          <div className={"document-edit"}>
-            <textarea value={text} onChange={onTextChange3}></textarea>
-          </div>
-        )}
-        <div
-          className={"document-markdown" + (!editing ? " editing-markdon" : "")}
-        >
-          <ReactMarkdown remarkPlugins={[gfm]} children={text}></ReactMarkdown>
-        </div>
-      </div>
-      {currentUser && editing && (
-        <div>
-          <a href="https://demo.hedgedoc.org/features?both">
-            {" "}
-            Help and instructions
-          </a>
-        </div>
+    <Container className="document-container-wrapper">
+      {currentUser && (
+        <Row className="document-buttons">
+          <Col lg={9} md={6} xs={11}>
+            <Toast
+              onClose={() => setShowToastSaved(false)}
+              show={showToastSaved}
+              delay={1000}
+              autohide
+            >
+              <Toast.Body>Changes saved!</Toast.Body>
+            </Toast>
+          </Col>
+          <Col lg={3} md={6} xs={12} className="document-edit-button">
+            <Button
+              variant="outline-secondary"
+              onClick={() => toggleEditing(!editing)}
+            >
+              {!editing ? "edit" : "stop editing and save"}
+            </Button>
+          </Col>
+        </Row>
       )}
-    </div>
+      <Row>
+        <Col xs></Col>
+      </Row>
+      <Row className="document-container">
+        {currentUser && editing && (
+          <Col lg={6} md={12} xs={12}>
+            {currentUser && editing && (
+              <div className={"document-edit"}>
+                <textarea value={text} onChange={onTextChange3}></textarea>
+              </div>
+            )}
+          </Col>
+        )}
+        <Col lg={1}></Col>
+        <Col lg={currentUser && editing ? 5 : 12} md={12} xs={12}>
+          <div
+            className={"vision-markdown" + (!editing ? " editing-markdon" : "")}
+          >
+            <ReactMarkdown
+              remarkPlugins={[gfm]}
+              children={text.replace(re, "")}
+            ></ReactMarkdown>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
